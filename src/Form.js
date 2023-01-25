90c0ba5999d82d2578f6d0719cc8a11760760a2e
Sadakshya Sharma <sadakshya.27s@gmail.com>
2023-01-25 13:47:17 +0530

    added default structure

--- src/Form.js
+++ src/Form.js
@@ -1,40 +1,42 @@
-import React from 'react'
-import { useState,useRef } from 'react';
-import axios from 'axios'
-import addNewProfile from './addNewProfile';
-import Card from './Card';
+import React from "react";
+import { useState, useRef } from "react";
+import axios from "axios";
+import addNewProfile from "./addNewProfile";
+import Card from "./Card";
 
 const Form = () => {
-    const [UserName, setUserName] = useState("");
+  const [UserName, setUserName] = useState("");
   const inputRef = useRef("");
   const [profile, setProfile] = useState(null);
-  
-  
+
   async function handleSubmit(event) {
-        event.preventDefault();
-       const resp = await axios.get(
-      `https://api.github.com/users/${inputRef.current.value}`);
-       console.log('App',resp.data);
-    inputRef.current.value = ""
-    
+    event.preventDefault();
+    const resp = await axios.get(
+      `https://api.github.com/users/${inputRef.current.value}`
+    );
+    console.log("App", resp.data);
+    inputRef.current.value = "";
+
     setProfile(resp.data);
-    };
+  }
 
   return (
     <>
       <form className="form">
         <input
+          className="formbutton"
           type="text"
           placeholder="GitHub username"
           ref={inputRef}
           required
           onChange={(e) => setUserName(e.target.value)}
         />
-        <button onClick={handleSubmit}>Add Card</button>
+        <button className="formbutton" onClick={handleSubmit}>
+          Add Card
+        </button>
       </form>
       <Card profile={profile} />
     </>
   );
-}
+};
 export default Form;
-
